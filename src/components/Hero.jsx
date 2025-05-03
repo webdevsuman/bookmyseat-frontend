06ba2f2e540aff70c0c6e52db534d6e0f98f8cdb
Suman Das <sumandas1995@yahoo.com>
2025-05-03 11:03:26 +0530

    Welcome page fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="text-center mt-30 md:mt-40 flex justify-center items-center">
+    <div id="hero" className="text-center mt-30 md:mt-40 flex justify-center items-center">
       <div className=" bg-gray-800 md:py-20 md:px-30 p-8 rounded-2xl">
         <h1 className="text-3xl text-blue-600 mb-5">Welcome</h1>
         <p className="text-gray-400">Login by clicking the Sign in button</p>
